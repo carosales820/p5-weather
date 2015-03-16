1d32f0175966c0d0fb7b933a44b230fd780587ac
carosales820 <carosales.820@gmail.com>
2015-03-16 10:28:23 -0700

    clean up

--- js/scripts.js
+++ js/scripts.js
@@ -25,44 +25,30 @@ $("#getWeather").blur( function() {
 $('#getZip').click(function() {
   $('.form').hide("slow");
 
-  // Stop Default
+
   event.preventDefault();
-  
-  // 1. Get & store entered zipcode
+
   var zipcode = $('#getWeather').val();
   console.log(zipcode);
-  
-  // 2. Pass weather into _simpleWeather()_ object
+
   $.simpleWeather({
     
     location: zipcode,
   
     success: function(weather) {
-      
-      // Get & store temperature
+
       var temp = weather.temp;
-      // Get & store city
       var city = weather.city;
-      // Get & store state
       var state = weather.region;
-      // Get & store text
       var text = weather.text;
-      // Get & store current weather
       var current = weather.currently;
-      // Get & store high
       var high = weather.high;
-      // Get & store low
       var low = weather.low;
-      // Get & store sunrise
       var sunrise = weather.sunrise;
-      // Get & store sunset
       var sunset = weather.sunset;
-      // Get & Store Update Info
       var updated = weather.updated;
-      // Get & Store Code
       var code = weather.code;
-      
-      // Output to hooks in HTML
+
       $('.temp').text(temp + "°" + "F");
       $('.city').text(city + ',' + ' ' + state);
       $('.state').text(state);
@@ -74,7 +60,6 @@ $('#getZip').click(function() {
       $('.sunset').text(sunset);
       $('.update').text(updated);
 
-      // See console for all properties of object
       console.log(weather);
     },
   
@@ -84,7 +69,6 @@ $('#getZip').click(function() {
   
   });
   
-  // 3. Reset input value
   $('#getWeather').val('');
   
 });
